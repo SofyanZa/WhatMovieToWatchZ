3a3b024b66ed06e993976e93c9b3647253324059
SofyanZa <soza@live.fr>
2022-11-06 23:02:08 +0100

    add Cast&Movie Model

---
+++ WhatMovieToWatchZ/Models/CastModel.swift
@@ -0,0 +1,14 @@
+//
+//  CastModel.swift
+//  Info Movie
+//
+//  Created by Sofyan Zarouri  on 23/08/2022.
+//
+
+import Foundation
+import UIKit
+
+struct CastModel {
+    let castImage: UIImage
+    let castName: String
+}
---
+++ WhatMovieToWatchZ/Models/MovieModel.swift
@@ -0,0 +1,16 @@
+//
+//  MovieModel.swift
+//  Info Movie
+//
+//  Created by Sofyan Zarouri  on 23/08/2022.
+//
+
+import Foundation
+import UIKit
+
+struct MovieModel {
+    let posterImage: UIImage
+    let id: Int
+    let currentPage: Int
+    let totalPages: Int
+    }
