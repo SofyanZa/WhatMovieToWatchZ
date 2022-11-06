e77f18d120f5ed8f0ba381ea81412be48b270bd0
SofyanZa <soza@live.fr>
2022-11-06 22:58:20 +0100

    adding constants keys tmbd api

---
+++ WhatMovieToWatchZ/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Info Movie
+//
+//  Created by Sofyan Zarouri  on 23/08/2022.
+//
+
+import Foundation
+
+/// Structure K regroupant le nom de l'app et la clé api de TMBD
+struct K {
+    static let appName = "WhatMovieToWatch"
+    static let apiKey = "747186b06fc6329c3b034129295297fa"
+}
+
+// let baseUrl = "https://api.themoviedb.org/3/"
