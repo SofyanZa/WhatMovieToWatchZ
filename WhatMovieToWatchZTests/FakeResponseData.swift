365ddd46e933e751ed00769ebc268ccb208a2575
SofyanZa <soza@live.fr>
2022-11-07 01:10:55 +0100

    adding tests unit

---
+++ WhatMovieToWatchZTests/FakeResponseData.swift
@@ -0,0 +1,35 @@
+//
+//  FakeResponseData.swift
+//  Info Movie
+//
+//  Created by Sofyan Zarouri  on 23/08/2022.
+//
+
+import Foundation
+
+class FakeResponseData {
+    // MARK: - Response
+    /// Fausse bonne reponse 200
+    static let responseOK = HTTPURLResponse(
+        url: URL(string: "https://apple.com")!,
+        statusCode: 200, httpVersion: nil, headerFields: [:])!
+    /// Fausse mauvaise reponse 500
+    static let responseKO = HTTPURLResponse(
+        url: URL(string: "https://apple.com")!,
+        statusCode: 500, httpVersion: nil, headerFields: [:])!
+
+    // MARK: - Data
+    /// Les fausses bonnes données issues du fichier json movies
+    static var movieCorrectData: Data? {
+        let bundle = Bundle(for: FakeResponseData.self)
+        let url = bundle.url(forResource: "movies", withExtension: "json")!
+        return try? Data(contentsOf: url)
+    }
+    
+    /// Fausse mauvaise donnée
+    static let movieIncorrectData = "erreur".data(using: .utf8)!
+    
+    // MARK: - Error
+    class testError: Error {}
+    static let error = testError()
+}
---
+++ WhatMovieToWatchZTests/WhatMovieToWatchZTests.swift
@@ -0,0 +1,100 @@
+//
+//  WhatMovieToWatchZTests.swift
+//  WhatMovieToWatchZTests
+//
+//  Created by SofyanZ on 07/11/2022.
+//
+
+import XCTest
+@testable import WhatMovieToWatchZ
+
+final class MovieManagerTest: XCTestCase {
+    // test if callback returns an error
+    func testMovieManagerShouldPostFailedCallbackIfError() {
+        
+        //Given
+        let session = URLSessionFake(data: nil, response: nil, error: FakeResponseData.error)
+        let movieManager = MovieManager(session: session)
+
+        //When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        movieManager.fetchMovies(page: 1, sort: "") { result in
+            // Then
+            guard case .failure(let error) = result else {
+                XCTFail("Test request method with an error failed")
+                return
+            }
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+        
+    }
+    
+    // test if callback doesn't return data
+    func testMovieManagerShouldPostFailedCallbackIfNoData() {
+        
+        //Given
+        let session = URLSessionFake(data: nil, response: nil, error: nil)
+        let movieManager = MovieManager(session: session)
+
+        //When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        movieManager.fetchMovies(page: 1, sort: "") { result in
+            // Then
+            guard case .failure(let error) = result else {
+                XCTFail("Test request method with an error failed")
+                return
+            }
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+        
+    }
+    
+    
+    // test if callback returns incorrect data
+    func testMovieManagerShouldPostFailedCallbackIncorrectData() {
+        
+        //Given
+        let session = URLSessionFake(data: FakeResponseData.movieIncorrectData, response: FakeResponseData.responseOK, error: nil)
+        let movieManager = MovieManager(session: session)
+
+        //When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        movieManager.fetchMovies(page: 1, sort: "") { result in
+            // Then
+            guard case .failure(let error) = result else {
+                XCTFail("Test request method with an error failed")
+                return
+            }
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+        
+    }
+    
+    // test if callback returns success with no error and correct data
+    func testMovieManagerShouldPostSuccessCallbackIfNoErrorAndCorrectData() {
+        
+        //Given
+        let session = URLSessionFake(data: FakeResponseData.movieCorrectData, response: FakeResponseData.responseOK, error: nil)
+        let movieManager = MovieManager(session: session)
+
+        //When
+        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        movieManager.fetchMovies(page: 1, sort: "") { result in
+            // Then
+            guard case .success(_) = result else {
+                XCTFail("Test request method with an error failed")
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.01)
+        
+    }
+}
